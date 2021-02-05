46a5c9cf7b1898e7840bcf13f3e06d62704b6608
Domas Pocius <domas.pocius@gmail.com>
2021-02-05 10:59:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+
+//----------------STRINGAI--------------
+
+/*
+ * PHP pagrindai
+ */
+//$name = "Martynas";
+//var_dump($name); // parodo funkcija
+//$lastName = "Kaselionis";
+//$pavadinimas = "Terminatorius";
+//$data = "2021.03.12";
+//$info = $pavadinimas;
+//$info .= $data;
+//$name =''; // perasymas
+//echo "Hello world"; // isvedimas
+//echo "Mano vardas".$name;
+//echo 12;
+//echo "Mano vardas $name";
+//echo "Filmo pavadinimas: $pavadinimas. ir ji rodys .$data";
+
+//$eilute = "Labas rytas, Lietuva!";
+
+//$ilgis = strlen($eilute);// eilutes simoboliu skaicius
+//$eilute_be_tarpu = trim($eilute);//grazina eilute be tarpu pradzioje ir pabaigoje
+//$didziosios_raides = strtoupper($eilute); // pakeicia visas raides i didziasias
+//$mazosios_raides = strtolower($eilute); // pakeicia visas raides i mazasias
+//$pavadinimas = ucfirst($eilute);// pakeicia tik pirma eilutes raide i didziaja
+//$tekstas = str_replace("Lietuva", "Latvija", $eilute);// pakeicia zodi is vieno i kita
+//$zodis = substr($eilute, 1, 5);// grazina zodzio dali, siuo atveju 5 simbolius pradedant nuo antro
+//$text = "\t\tPHP yra puiki programavimo kalba ;) ... ";
+//$trimed = trim($text);// pasalina tarpus string pradzioje ir pabaigoje
+//$kaina= 12;
+//$kiekis = 5;
+//$suma = $kaina * $kiekis;
+//$suma *=1.25;// tarkim pvm
+
+
+//echo is_int($kiekis);
+
+//echo $kiekis++; // 5 post increment
+//echo $kiekis;//6
+//echo ++$kiekis; // output 5 pre increment
+
+//---------------------Masyvai------------------
+//$items = ['Preke',12, true,67.78 ];
+
+//var_dump($items);
+
+//echo $items[0];// isveda pirma preke
+//print_r($items)// atspaudina struktura
+
+//echo count($items);//skaiciuojam elementu skaiciu masyve
+
+//$items = [
+    //12=>[
+    //    "Kompiuteris lenovo",
+  //      "Macbook"
+//],
+  //  456=> 'Telefonas',
+ //   67=> 'Piestukas',
+//]; indeksuotas masyvas
+
+//echo $items[12][1];
+
+//$items =[
+//    'Kompiuteriai'=> [
+//        123=> "Lenovo",
+//        673=> "Apple Macbook"
+//    ],
+//    'Telefonai'=>[
+//        6789=>"Iphone"
+ //   ]
+//];
+//echo $items["Kompiuteriai"][123];
+
+
+//$studentai = [
+//    '1c:'=>[
+//        "1pogrupis:"=>[
+//            1=>"Petras",
+//            2=>"Jonas",
+//            3=>"Ieva",
+//            ]
+//    ],
+//    '2c:'=>[
+//        "2pogrupis:"=>[
+//            1=>"Karolis",
+//            2=>"Tomas",
+//            3=>"Simas",
+//        ]
+//]
+//];
+//echo count($studentai);
+
+//--------Masyvas i stringa ir atgal-------
+
+//$items = [
+//    123=> "Lenovo",
+//    678=>"Applle",
+//    6789=> "iphone",
+//];
+
+
+
+//$items = implode(',',$items);// masyva konvertuojam i eilute
+//$items = explode(',',$items);// eilute konvertuojam i masyva
+
+
+?>
+
+
+
+
+
+
+
+
+
